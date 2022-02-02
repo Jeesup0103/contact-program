80356366b15aa2ccb911587a847e02b9a8755bd8
sup <jeesup0103@gmail.com>
2022-02-02 22:34:16 +0900

    first commit

---
+++ contacts.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+
+using namespace std;
+
+int main() {
+    //vector<tuple<string, string, string>> v;
+    //name, phone number, email
+    cout << "#############CONTACTS################\n";
+    string name, pn, email;
+
+    cout << "Type you name \n";
+    cin >> name;
+
+    cout << "What is your number?\n";
+    cin >> pn;
+
+    cout << "What is your email?\n";
+    cin >> email;
+
+
+}
